d01372a4c9845ac418a719b18f140c1a5b491f53
grino5 <grino.contacto@gmail.com>
2024-10-10 00:18:16 -0400

    Tienda

---
+++ app/page.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import WebApp from "@twa-dev/sdk";
+
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
+
+export default function Home() {
+  const [userData, setUserData] = useState<UserData | null>(null)
+
+  useEffect(() => {
+    // Inicializa WebApp
+    WebApp.ready();
+
+    // Verifica si hay datos de usuario disponibles
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData)
+    }
+  }, [])
+
+  return (
+    <main className="p-4">
+      {
+        userData ?
+          (
+            <>
+              <h1 className="text-2xl font-bold mb-4">User Data</h1>
+              <ul>
+                <li>ID: {userData.id}</li>
+                <li>First Name: {userData.first_name}</li>
+                <li>Last Name: {userData.last_name}</li>
+                <li>Username: {userData.username}</li>
+                <li>Language Code: {userData.language_code}</li>
+                <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+              </ul>
+            </>
+          ) :
+          (
+            <div>Loading...</div>
+          )
+      }
+    </main>
+  );
+}
